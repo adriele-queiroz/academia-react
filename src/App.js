1509f230be7b4327be2908219d4c5ff3436f9ad7
Adriele Queiroz <105941726+adriele-queiroz@users.noreply.github.com>
2024-05-21 13:06:48 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React from 'react';
 import './index.css'; 
 
-// Importando os componentes que compõem a aplicação
 import NavBar from './components/NavBar';
 import FeaturesSection from './components/FeaturesSection';
 import HeroSection from './components/HeroSection';
@@ -11,24 +9,13 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      {/* Componente da barra de navegação */}
       <NavBar />
-
-      {/* Componente da seção de hero */}
       <HeroSection />
-
-      {/* Componente da seção de recursos */}
       <FeaturesSection />
-
-      {/* Componente da seção de depoimentos */}
       <TestimonialsSection />
-      
-      {/* Componente da seção de depoimentos */}
       <Footer />
-      
     </div>
   );
 }
 
 export default App;
-
